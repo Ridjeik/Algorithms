5151a680203336dd7ec231afb6c8803394c5c965
Артем Марущак <artemmaruschak12@gmail.com>
2021-10-01 18:42:24 +0300

    Added cyclic gcd realization

--- euclid_gcd.c
+++ euclid_gcd.c
@@ -2,13 +2,26 @@
 #include <stdlib.h>
 
 long long int gcd_recursive(long long int x, long long int y);
+long long int gcd_cycle(long long int x, long long int y);
 
 int main(int argc, char const *argv[])
 {
-    printf("%lld", gcd_recursive(atol(argv[1]), atol(argv[2])));
+    printf("Recursive: %lld\n", gcd_recursive(atol(argv[1]), atol(argv[2])));
+    printf("Cyclic: %lld", gcd_cycle(atol(argv[1]), atol(argv[2])));
     return 0;
 }
 
+long long int gcd_cycle(long long int x, long long int y)
+{
+    long long temp = 0;
+    while(y){
+        x %= y;
+        temp = x;
+        x = y;
+        y = temp;
+    }
+    return x;
+}
 
 long long int gcd_recursive(long long int x, long long int y)
 {
