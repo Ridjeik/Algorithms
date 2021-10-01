0eec4038097f3960b3fddd39a3d16c22d683b058
Артем Марущак <artemmaruschak12@gmail.com>
2021-10-01 22:28:29 +0300

    Added linear and matrix fibonacci numbers realizations

---
+++ fibonacci.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long long int fib_matrix(int n);
+long long int fib_linear(int n);
+
+int main(int argc, char const *argv[])
+{
+    printf("Linear: %lld\n",fib_linear(atoi(argv[1])));
+    printf("Matrix: %lld",fib_matrix(atoi(argv[1])));
+    return 0;
+}
+
+long long int fib_linear(int n){
+    long long int n1 = 1, n2 = 1;
+    for(int i = 3; i <= n; i++){
+        if(n2 >= n1) n1 += n2;
+        else n2 += n1;  
+    }
+    return n1 > n2 ? n1 : n2;
+    
+}
+
+long long int fib_matrix(int n)
+{
+    long long int result_a = 0, result_b = 1,
+        a = 0, b = 1, ta = a, tb = b,
+        c = 1, d = 1, tc = c,
+        temp_result_a = result_a;
+    
+    while (n)
+    {
+        if(n & 1){
+            temp_result_a = result_a;
+            result_a = result_a * a + result_b * c;
+            result_b = temp_result_a * b + result_b * d;  
+        }
+
+        ta = a;
+        tb = b;
+        tc = c;
+        a = a*a + b*c;
+        b = ta*b + b*d;
+        c = c*ta + d*c;
+        d = tc*tb + d*d;
+
+        n >>= 1;
+    }
+    return result_a;
+}
